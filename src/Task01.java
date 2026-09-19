a518a2f3ab3611b36fc3a4c77b3daf77893aecf3
nahomair716 <nahomair716@gmail.com>
2026-09-19 10:50:38 -0400

    Update Task02.java

--- src/Task01.java
+++ src/Task01.java
@@ -1,2 +1,19 @@
-public class Task01 {
+public class Task02 {
+    public void main(){
+        int springMaintenanceCost = 288;
+        int summerMaintenanceCost = 498;
+        int fallMaintenanceCost = 215;
+        int winterMaintenanceCost = 343;
+        int cost = springMaintenanceCost + summerMaintenanceCost + fallMaintenanceCost + winterMaintenanceCost;
+
+
+        System.out.println("The Spring maintenance cost is $" + springMaintenanceCost);
+        System.out.println("The Summer maintenance cost is $" + summerMaintenanceCost);
+        System.out.println("The Fall maintenance cost is $" + fallMaintenanceCost);
+        System.out.println("The Winter maintenance cost is $" + winterMaintenanceCost);
+        System.out.println("The toal yearly maintenance cost is $" + cost);
+
+
+    }
+
 }
